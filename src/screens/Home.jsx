fa7e03c705cd3c581efc1d3f7aaa8c2ea21c0ec1
Thaisa Lima <thaisafc.lima@outlook.com>
2022-03-22 17:42:48 +0100

    finished the order items component

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,25 +2,28 @@ import homeImage from "../assets/images/home-image.png";
 import "../styles/screens/Home.css";
 
 export default function Home() {
+  function onParcelIDHandler() {}
+
   return (
     <div>
       <section className="hero-wrapper">
         <h1>DachsHunt</h1>
         <h1>We hunt your package as if it were a badger.</h1>
       </section>
-      <section className="home-body-wrapper">
+      <form className="home-body-wrapper">
         <h3>Please enter a Parcel ID or your phone number</h3>
         <label>
           Parcel ID
-          <input type="number" />
+          <input type="number" onChange={onParcelIDHandler} />
         </label>
+        <h3>OR</h3>
         <label>
           Phone number
           <input type="number" />
         </label>
-        <button>Track</button>
-        <img src={homeImage} alt="a white dog sniffing the footer" />
-      </section>
+        <button type="submit">Track</button>
+      </form>
+      <img src={homeImage} alt="a white dog sniffing the footer" />
     </div>
   );
 }
