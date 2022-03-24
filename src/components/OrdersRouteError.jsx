eb3389e3aeb4b86e88e94e05d5d86df0b98398da
Thaisa Lima <thaisafc.lima@outlook.com>
2022-03-24 10:18:57 +0100

    finished orders item

--- src/components/OrdersRouteError.jsx
+++ src/components/OrdersRouteError.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import "../styles/components/OrdersRouteError.css";
 
 export default function OrdersRouteError() {
   return (
-    <section>
+    <section className="orders-route-error-wrapper">
       <h2>
         Oops, it looks like you are trying to access a page that requires some
         data. To solve this problem, you need to go back to our home page and
