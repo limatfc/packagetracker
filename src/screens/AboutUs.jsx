4e2a66b3ef9c4e4124f79282145083e4a5cf064b
Thaisa Lima <thaisafc.lima@outlook.com>
2022-03-24 14:13:04 +0100

    finished css

--- src/screens/AboutUs.jsx
+++ src/screens/AboutUs.jsx
@@ -1,3 +1,23 @@
+import image from "../assets/images/about-image.png";
+import "../styles/screens/AboutUs.css";
+
 export default function AboutUs() {
-  return <div>Hello</div>;
+  return (
+    <section className="about-wrapper">
+      <h2>About us</h2>
+      <p>
+        Founded in 2022, DachsHunt is a Sweden-based tech company with the
+        mission to make online shopping easier. Through our extensive network of
+        parcel lockers and home delivery services, DachsHunt serves a great
+        number of people in both Sweden and Brazil.
+      </p>
+      <p>
+        Our unique DachsHunt culture helps us relentlessly pursue our mission of
+        being Earth’s most customer-centric company, best employer, and safest
+        place to work. Also, we have made the commitment to better the lives of
+        the greatest number of people possible though our services.{" "}
+      </p>
+      <img src={image} alt="a white dog walking, wearing black sunglasses" />
+    </section>
+  );
 }
