9e40bcebb54e5135e5c1aab4ebfa9367113f705b
Thaisa Lima <thaisafc.lima@outlook.com>
2022-03-24 14:41:48 +0100

    finished with styles

--- src/components/Error.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/components/Error.css";
-
-export default function Error() {
-  return (
-    <div className="error-wrapper">
-      SORRY, THERE HAS BEEN AN ERROR IN OUR SYSTEM. PLEASE TRY AGAIN.
-    </div>
-  );
-}
